1b307a76e3bb29fcaa8039d6da762b559699313c
OLD_RD3黃祥恩 <shiangenhuang@igs.com.tw>
2026-03-15 23:19:40 +0800

    feat: enhance arrow func

---
+++ assets/md-C-MmEJdg.js
@@ -0,0 +1,23 @@
+import{_ as k}from"./slidev/CodeBlockWrapper.vue_vue_type_script_setup_true_lang-Cy-WnKKo.js";import{o as p,b as h,w as e,g as s,d as o,m as d,ad as i,v as c,x as g,T as n}from"./modules/vue-DZBKmgnn.js";import{I as D}from"./slidev/default-BTCHHsYz.js";import{u as m,f as y}from"./slidev/context-D1RbB5j4.js";import"./modules/unplugin-icons-B-aHGt_R.js";import"./index-BudC-oKR.js";import"./modules/shiki-B9ns20UJ.js";import"./slidev/TitleIcon.vue_vue_type_script_setup_true_lang-BE5lCulH.js";const L={__name:"slides.md__slidev_38",setup(B){const{$clicksContext:l,$frontmatter:t}=m();return l.setup(),(E,a)=>{const r=k;return p(),h(D,c(g(n(y)(n(t),37))),{default:e(()=>[a[1]||(a[1]=s("p",null,[s("strong",null,"改善範例：")],-1)),o(r,d({},{title:"",ranges:[]}),{default:e(()=>[...a[0]||(a[0]=[s("pre",{class:"shiki shiki-themes vitesse-dark vitesse-light slidev-code",style:{"--shiki-dark":"#dbd7caee","--shiki-light":"#393a34","--shiki-dark-bg":"#121212","--shiki-light-bg":"#ffffff"}},[s("code",{class:"language-diff"},[s("span",{class:"line"},[s("span",{style:{"--shiki-dark":"#FDAEB7","--shiki-light":"#B31D28"}},"- export enum ItemModelShowMode {")]),i(`
+`),s("span",{class:"line"},[s("span",{style:{"--shiki-dark":"#FDAEB7","--shiki-light":"#B31D28"}},"-     Default = 0,")]),i(`
+`),s("span",{class:"line"},[s("span",{style:{"--shiki-dark":"#FDAEB7","--shiki-light":"#B31D28"}},"-     SpriteFrame = 1,")]),i(`
+`),s("span",{class:"line"},[s("span",{style:{"--shiki-dark":"#FDAEB7","--shiki-light":"#B31D28"}},"- }")]),i(`
+`),s("span",{class:"line"},[s("span",{style:{"--shiki-dark":"#85E89D","--shiki-light":"#22863A"}},"+ export type ItemModelShowMode = 0 | 1;")]),i(`
+`),s("span",{class:"line"},[s("span",{style:{"--shiki-dark":"#85E89D","--shiki-light":"#22863A"}},"+ export const ItemModelShowMode = {")]),i(`
+`),s("span",{class:"line"},[s("span",{style:{"--shiki-dark":"#85E89D","--shiki-light":"#22863A"}},"+     Default: 0 as const,")]),i(`
+`),s("span",{class:"line"},[s("span",{style:{"--shiki-dark":"#85E89D","--shiki-light":"#22863A"}},"+     SpriteFrame: 1 as const,")]),i(`
+`),s("span",{class:"line"},[s("span",{style:{"--shiki-dark":"#85E89D","--shiki-light":"#22863A"}},"+ };")]),i(`
+`),s("span",{class:"line"}),i(`
+`),s("span",{class:"line"},[s("span",{style:{"--shiki-dark":"#FDAEB7","--shiki-light":"#B31D28"}},"- export enum TraceLevel {")]),i(`
+`),s("span",{class:"line"},[s("span",{style:{"--shiki-dark":"#FDAEB7","--shiki-light":"#B31D28"}},"-     Trace = 0,")]),i(`
+`),s("span",{class:"line"},[s("span",{style:{"--shiki-dark":"#FDAEB7","--shiki-light":"#B31D28"}},"-     Log = 1,")]),i(`
+`),s("span",{class:"line"},[s("span",{style:{"--shiki-dark":"#FDAEB7","--shiki-light":"#B31D28"}},"-     Warning = 2,")]),i(`
+`),s("span",{class:"line"},[s("span",{style:{"--shiki-dark":"#FDAEB7","--shiki-light":"#B31D28"}},"-     Error = 3,")]),i(`
+`),s("span",{class:"line"},[s("span",{style:{"--shiki-dark":"#FDAEB7","--shiki-light":"#B31D28"}},"- }")]),i(`
+`),s("span",{class:"line"},[s("span",{style:{"--shiki-dark":"#85E89D","--shiki-light":"#22863A"}},"+ export type TraceLevel = 0 | 1 | 2 | 3;")]),i(`
+`),s("span",{class:"line"},[s("span",{style:{"--shiki-dark":"#85E89D","--shiki-light":"#22863A"}},"+ export const TraceLevel = {")]),i(`
+`),s("span",{class:"line"},[s("span",{style:{"--shiki-dark":"#85E89D","--shiki-light":"#22863A"}},"+     Trace: 0,")]),i(`
+`),s("span",{class:"line"},[s("span",{style:{"--shiki-dark":"#85E89D","--shiki-light":"#22863A"}},"+     Log: 1,")]),i(`
+`),s("span",{class:"line"},[s("span",{style:{"--shiki-dark":"#85E89D","--shiki-light":"#22863A"}},"+     Warning: 2,")]),i(`
+`),s("span",{class:"line"},[s("span",{style:{"--shiki-dark":"#85E89D","--shiki-light":"#22863A"}},"+     Error: 3,")]),i(`
+`),s("span",{class:"line"},[s("span",{style:{"--shiki-dark":"#85E89D","--shiki-light":"#22863A"}},"+ } as const;")])])],-1)])]),_:1},16)]),_:1},16)}}};export{L as default};
